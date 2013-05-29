7a06c60c7f19a999094ea42b0215828018808ef7
viniciusrdacosta <viniciusrdacosta@gmail.com>
2013-05-29 06:28:42 -0700

    Início do Projeto Refuel

---
+++ refuel/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Refuel::Application.config.secret_token = 'e4f51dcb9beeadd5ea4b3045dce91e298efb58e9dbda29fadaa626749a45cd041a37face7b70ca8c66168ce701fa688695aadc8116bcba7bdd8598ae3229986c'
